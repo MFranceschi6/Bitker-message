b85b416215bc3fe14ffe1effd7a8823c1b4b009e
Polletz <35771342+Polletz@users.noreply.github.com>
2018-09-30 15:44:38 +0200

    Update GetHeaders.java

--- src/main/java/com/bitkermessage/client/messages/messages/GetHeaders.java
+++ src/main/java/com/bitkermessage/client/messages/messages/GetHeaders.java
@@ -1,10 +1,16 @@
 package com.bitkermessage.client.messages.messages;
 
+import java.util.List;
+
 /**
  * Created by Matteo on 07/10/2016.
  */
 public class GetHeaders extends GetBlocks {
 
+    public GetHeaders(List<byte []> h){
+        super(h);
+    }
+    
     @Override
     public String getCommand() {
         return "getheaders";
